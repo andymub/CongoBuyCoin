b768c54ae3296db221f38d7a6cfc6a41cf7a0d32
Andymub <andymub1@gmail.com>
2018-07-08 22:56:51 +0100

    3 Com

--- app/src/main/java/coin/congobuy/com/Ui/Fragment/FragmentAchats.java
+++ app/src/main/java/coin/congobuy/com/Ui/Fragment/FragmentAchats.java
@@ -6,7 +6,9 @@ package coin.congobuy.com.Ui.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +16,23 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import coin.congobuy.com.R;
 import coin.congobuy.com.Ui.MainActivity;
-
-import static com.google.android.gms.flags.impl.SharedPreferencesFactory.getSharedPreferences;
 
 public class FragmentAchats extends Fragment implements View.OnClickListener {
     private String title;
@@ -34,6 +44,10 @@ public class FragmentAchats extends Fragment implements View.OnClickListener {
     public Button btnAchetr, btnVendre;
     public EditText editText_price;
     public Spinner Achatspinner;
+    public FirebaseFirestore db;
+    public String pricebit,bitcoinvalue;
+    int existe = 0;
+
 
     public static FragmentAchats newInstance(String title, int resImage) {
         FragmentAchats fragment = new FragmentAchats();
@@ -59,6 +73,7 @@ public class FragmentAchats extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_achat, container, false);
          final TextView tvLabel = (TextView) view.findViewById(R.id.txtMain);
         tvLabel.setText(title);
+        db = FirebaseFirestore.getInstance();
         Button btnAchat= view.findViewById(R.id.btnachat);
         Achatspinner=view.findViewById(R.id.spinner);
         editText_price=view.findViewById(R.id.editText_price);
@@ -73,38 +88,54 @@ public class FragmentAchats extends Fragment implements View.OnClickListener {
 
                     value=(value*2);
                     editText_price.setText(value.toString());
+                    pricebit="achat40";
+                    bitcoinvalue=value.toString();
 
                 }
                else if (position==0){//20
                    value=(value*1);
                     editText_price.setText(value.toString());
+                    pricebit="achat20";
+                    bitcoinvalue=value.toString();
 
                 }
                 else if (position==2){//50
                     value=(value*2.5);
                     editText_price.setText(value.toString());
+                    pricebit="achat50";
+                    bitcoinvalue=value.toString();
 
                 }
                 else if (position==3){//100
                     value=(value*5);
                     editText_price.setText(value.toString());
+                    pricebit="achat100";
+                    bitcoinvalue=value.toString();
 
                 }else if (position==4){//200
                     value=(value*10);
                     editText_price.setText(value.toString());
+                    pricebit="achat200";
+                    bitcoinvalue=value.toString();
 
                 }else if (position==5){//300
                     value=(value*15);
                     editText_price.setText(value.toString());
+                    pricebit="achat300";
+                    bitcoinvalue=value.toString();
 
                 }else if (position==6){//400
                     value=(value*20);
                     editText_price.setText(value.toString());
+                    pricebit="achat400";
+                    bitcoinvalue=value.toString();
 
                 }
-                else if (position==7){//400
+                else if (position==7){//500
                     value=(value*25);
                     editText_price.setText(value.toString());
+                    pricebit="achat500";
+                    bitcoinvalue=value.toString();
 
                 }
 
@@ -121,18 +152,22 @@ public class FragmentAchats extends Fragment implements View.OnClickListener {
         btnAchat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String simpleV="";
-
+                String user="",phone="";
+                String [] valeurAchat ={pricebit,bitcoinvalue};
 
                 if (sharedpreferences.contains(Email)) {
-                    simpleV=(sharedpreferences.getString(Email, "MyEmail"));
+                    user=(sharedpreferences.getString(Email, "MyEmail"));
 
                 } if (sharedpreferences.contains(Number)) {
-                    simpleV=simpleV+"_"+(sharedpreferences.getString(Number, "000"));
+                    phone=(sharedpreferences.getString(Number, "000"));
                 }
 
+                if ((sharedpreferences.getString(Number, ""))!=null ||(sharedpreferences.getString(Number, "")!=""))
+                {
+                    addNewCommande(user,phone,valeurAchat);
+                }
 
-                Toast.makeText(getContext(),"" +simpleV,Toast.LENGTH_SHORT).show();
+               // Toast.makeText(getContext(),"" +simpleV,Toast.LENGTH_SHORT).show();
 
 
             }
@@ -151,5 +186,112 @@ public class FragmentAchats extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
+    }
+    private void addNewCommande(String user,String number,String[] valeur) {
+        if ((ReadSingleUserPricePackage(user,valeur[0]))==1){
+            Toast.makeText(getContext(), "Veuillez valider conclure la commande précédente",
+
+                    Toast.LENGTH_SHORT).show();
+
+
+        }else {
+            UpdateData(user,valeur);
+        }
+
+            Map<String, Object> newPurshase = new HashMap<>();
+
+            newPurshase.put("Etat", "En cours de validation");
+
+
+            db.collection("Achats").document(user+"--"+number+"--"+valeur[0]).set(newPurshase)
+
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                        @Override
+
+                        public void onSuccess(Void aVoid) {
+
+                            Toast.makeText(getContext(), "Votre commande est en Cours de validation...",
+
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    })
+
+                    .addOnFailureListener(new OnFailureListener() {
+
+                        @Override
+
+                        public void onFailure(@NonNull Exception e) {
+
+                            Toast.makeText(getContext(), "ERROR" + e.toString(),
+
+                                    Toast.LENGTH_SHORT).show();
+
+                            Log.d("TAG", e.toString());
+
+                        }
+
+                    });
+
+    }
+
+    private int ReadSingleUserPricePackage(String userEmail, final String valeur) {
+        final String[] data = {null};
+
+        DocumentReference user = db.collection("User").document(userEmail);
+
+        user.get().addOnCompleteListener(new OnCompleteListener < DocumentSnapshot > () {
+
+            @Override
+
+            public void onComplete(@NonNull Task < DocumentSnapshot > task) {
+
+                if (task.isSuccessful()) {
+
+                    DocumentSnapshot doc = task.getResult();
+                    StringBuilder fields = new StringBuilder("");
+                    //fields.append("\nPhone: ").append(doc.get(valeur));
+                    //data[0] =doc.get(valeur).toString();
+                    if (doc.get(valeur).toString()==null){
+                        existe=1;
+                    }
+
+                }
+
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+
+                    @Override
+
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Hey",e.getMessage());
+
+                    }
+
+                });
+        return existe;
+
+    }
+    private void UpdateData(String user,String[] valeur) {
+
+        DocumentReference contact = db.collection("User").document(user);
+        contact.update(valeur[0], valeur[1])
+
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                    @Override
+
+                    public void onSuccess(Void aVoid) {
+
+                        Toast.makeText(getContext(), "Updated Successfully",
+
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+
+                });
     }
 }
--- app/src/main/java/coin/congobuy/com/Ui/Fragment/FragmentListCommandes.java
+++ app/src/main/java/coin/congobuy/com/Ui/Fragment/FragmentListCommandes.java
@@ -5,18 +5,67 @@ import coin.congobuy.com.R;
 /**
  * Created by Andymub on 04/07/2018.
  */
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class FragmentListCommandes extends Fragment {
     private String title;
     private int imageMain;
+    public FirebaseFirestore db;
     private int imageSecondary;
+    public ListView list = null;
+    SharedPreferences sharedpreferences;
+    public static final String mypreference = "CBCpref";
+    public static final String Number = "numKey";
+    public static final String Email = "emailKey";
+    private String[] mNames = { "Fabian", "Carlos", "Alex", "Andrea", "Karla",
+            "Freddy", "Lazaro", "Hector", "Carolina", "Edwin", "Jhon",
+            "Edelmira", "Andres" };
+    public final List<String> titre = new ArrayList<String>();
+
+    private String[] mAnimals = { "Perro", "Gato", "Oveja", "Elefante", "Pez",
+            "Nicuro", "Bocachico", "Chucha", "Curie", "Raton", "Aguila",
+            "Leon", "Jirafa" };
+    public int iFor=0;
+
+    int[] flags = new int[]{
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping,
+            R.drawable.ic_shopping
+    };
+    public ImageButton btnSync;
 
 
     public static FragmentListCommandes newInstance(String title, int resMainImage, int resSecondaryImage) {
@@ -41,6 +90,9 @@ public class FragmentListCommandes extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        db = FirebaseFirestore.getInstance();
+        sharedpreferences = this.getActivity().getSharedPreferences(mypreference, Context.MODE_PRIVATE);
         View view = inflater.inflate(R.layout.fragment_liste_commande, container, false);
         TextView tvLabel = (TextView) view.findViewById(R.id.txtMain);
         tvLabel.setText(title);
@@ -48,8 +100,126 @@ public class FragmentListCommandes extends Fragment {
         ImageView imageView = (ImageView) view.findViewById(R.id.imgMain);
         imageView.setImageResource(imageMain);
 
-        ImageView imageViewSecondary = (ImageView) view.findViewById(R.id.imgSecondary);
-        imageViewSecondary.setImageResource(imageSecondary);
-        return view;
+        ///ImageView imageViewSecondary = (ImageView) view.findViewById(R.id.imgSecondary);
+       // imageViewSecondary.setImageResource(imageSecondary);
+        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
+
+//        for(int i=0;i<10;i++){
+//            HashMap<String, String> hm = new HashMap<String,String>();
+//            hm.put("txt", "App Name : " + mNames[i]);
+//            hm.put("cur","creator : " + mAnimals[i]);
+//            hm.put("flag", Integer.toString(flags[i]) );
+//            aList.add(hm);}
+//        String[] from = { "flag","txt","cur" };
+//
+//        int[] to = { R.id.flag,R.id.txt,R.id.cur,R.id.textView2};
+
+        //View v = inflater.inflate(R.layout.fragment_top_rated, container,false);
+        list = (ListView)view.findViewById(R.id.listView1);
+        list.setVisibility(View.INVISIBLE);
+//        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.listview_layout, from, to);
+//        list.setAdapter(adapter);
+        btnSync= view.findViewById(R.id.btnlistSync);
+        btnSync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ReadAllCommande();
+                list.setVisibility(View.VISIBLE);
+            }
+        });
+
+            return view;
+    }
+    private void ReadAllCommande() {
+        String userEmail="",phone="";
+        if (sharedpreferences.contains(Email)) {
+            userEmail=(sharedpreferences.getString(Email, "MyEmail"));
+
+        } if (sharedpreferences.contains(Number)) {
+            phone=(sharedpreferences.getString(Number, "000"));
+        }
+        final String []value={"achat20","achat40","achat50","achat100","achat200","achat300","achat400","achat500"} ;
+
+        if ((userEmail!=null) ||(phone!=""))
+        {
+            int i=0;
+            String path=userEmail+"--"+phone+"--"+value[i];
+            for(i=0;i<8;i++){
+                iFor=i;
+                // Toast.makeText(getContext(),"" +simpleV,Toast.LENGTH_SHORT).show();
+
+
+                if(path=="ch@gmail.com--0998623313--achat20"){
+                    int u=0;
+                }
+                    DocumentReference user = db.collection("Achats").document(path);
+//                if (user.get().isSuccessful()) {
+                    int t;
+                    // do something with the data
+                    user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+
+                        @Override
+
+                        public void onComplete(@NonNull Task< DocumentSnapshot > task) {
+
+                            if (task.isSuccessful()) {
+
+                                DocumentSnapshot doc = task.getResult();
+
+//                            StringBuilder fields = new StringBuilder("");
+//
+//                            fields.append("Name: ").append(doc.get("Name"));
+//
+//                            fields.append("\nEmail: ").append(doc.get("Email"));
+//
+//                            fields.append("\nPhone: ").append(doc.get("Phone"));
+
+                                //titre.add(doc.get("Etat").toString());
+                                mNames[iFor]=value[iFor];
+                                mAnimals[iFor]=doc.get("Etat").toString();
+                                Toast.makeText(getContext(),"--"+doc.get("Etat").toString(),Toast.LENGTH_SHORT).show();
+                                titre.add(doc.get("Etat").toString());
+
+
+                            }
+
+                        }
+
+                    })
+
+                            .addOnFailureListener(new OnFailureListener() {
+
+                                @Override
+
+                                public void onFailure(@NonNull Exception e) {
+
+                                }
+
+                            });
+
+//                } else {
+//                    Toast.makeText(getContext(),"no data",Toast.LENGTH_SHORT).show();
+//                }
+
+            };
+
+        }
+
+        // imageViewSecondary.setImageResource(imageSecondary);
+        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
+
+        for(int i=0;i<10;i++){
+            HashMap<String, String> hm = new HashMap<String,String>();
+            hm.put("txt", "App Name : " + mNames[i]);
+            hm.put("cur","creator : " + mAnimals[i]);
+            hm.put("flag", Integer.toString(flags[i]) );
+            aList.add(hm);}
+        String[] from = { "flag","txt","cur" };
+
+        int[] to = { R.id.flag,R.id.txt,R.id.cur,R.id.textView2};
+        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.listview_layout, from, to);
+        adapter.notifyDataSetChanged();
+        list.setAdapter(adapter);
     }
 }
